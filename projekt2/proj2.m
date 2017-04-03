16700d3d0d8f751e42e2661678c98b52beaec7fc
Kristoffer Lundgren <kristoffer.lundgren@gmail.com>
2017-04-03 17:17:41 +0200

    .

--- projekt2/proj2.m
+++ projekt2/proj2.m
@@ -3,7 +3,7 @@ data = xlsread('pk', 1);
 n_samples = 10; % # of samples per patient
 n_patients = n/n_samples; % # of patients, assuming same number of samples per patient
 
-data_array = zeros(n_patients, n_samples); % dividing up the data, row is patient number
+data_array = zeros(n_patients, n_samples); % array for dividing up the data, row is patient number
 time = data(1:10, 2);
 
 for i = 1:n_patients
