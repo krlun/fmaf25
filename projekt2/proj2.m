3432b6671910379852f01af5dd36f8ac780cf559
Kristoffer Lundgren <kristoffer.lundgren@gmail.com>
2017-04-04 15:23:28 +0200

    oaa

--- projekt2/proj2.m
+++ projekt2/proj2.m
@@ -8,18 +8,18 @@ data_array = zeros(n_patients, n_samples); % array for dividing up the data, row
 time = data(1:10, 2);
 
 for i = 1:n_patients
-    data_array(i, 1:end) = data(((i-1)*10+1):(i*10), 3); % Radvis plasmakoncentration f??r patient 101..110
+    data_array(i, 1:end) = data(((i-1)*10+1):(i*10), 3); % Radvis plasmakoncentration for patient 101..110
 end
 
-data_mean = mean(data_array'); % Medelv??rde taget ??ver varje tidpunkt
-data_std = std(data_array'); % Standardavvikelse taget ??ver varje tidpunkt
-data_var = data_std.^2; % Varians taget ??ver varje tidpunkt
+data_mean = mean(data_array'); % Medelvarde taget over varje tidpunkt
+data_std = std(data_array'); % Standardavvikelse taget over varje tidpunkt
+data_var = data_std.^2; % Varians taget over varje tidpunkt
 
 subplot(2,1,1)
 plot(time, data_array, 'b')
 xlabel('Tid (h)')
 ylabel('Plasmakoncentration')
-title('Uppm??tt data')
+title('Uppmatt data')
 
 subplot(2,1,2)
 plot(time, data_mean, 'b') 
@@ -28,4 +28,4 @@ plot(time, data_mean + norminv(0.975)*data_std, 'r--');
 plot(time, data_mean - norminv(0.975)*data_std, 'r--');
 xlabel('Tid (h)')
 ylabel('Plasmakoncentration')
-title('Medelv??rde och 95% CI')
+title('Medelvarde och 95% CI')
