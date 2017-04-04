a8b38fbd362058c95de2cfb763ba8eab4c6720d7
krlun <kristoffer.lundgren@gmail.com>
2017-04-04 16:13:52 +0200

    optimering av konstanter

--- projekt2/proj2.m
+++ projekt2/proj2.m
@@ -5,7 +5,7 @@ n_samples = 10; % Antal samples per patient
 n_patients = 10; % Antal patienter
 
 data_array = zeros(n_patients, n_samples); % array for dividing up the data, row is patient number
-time = data(1:10, 2);
+time = data(1:10, 2)';
 
 for i = 1:n_patients
     data_array(i, 1:end) = data(((i-1)*10+1):(i*10), 3); % Radvis plasmakoncentration for patient 101..110
@@ -29,3 +29,7 @@ plot(time, data_mean - norminv(0.975)*data_std, 'r--');
 xlabel('Tid (h)')
 ylabel('Plasmakoncentration')
 title('Medelvarde och 95% CI')
+
+% constants: F, k_a, A, lambda, B, mu
+constants = rand(1, 6); % initial gissning av konstanter
+constants = fminsearch('Qfunc', constants, [], time, data_mean);
