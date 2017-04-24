034951d336aef22d3168114195639c32b93eac27
Kristoffer Lundgren <kristoffer.lundgren@gmail.com>
2017-04-24 15:58:37 +0200

    .

--- projekt2/superposition.m
+++ projekt2/superposition.m
@@ -5,7 +5,7 @@ function [ y_sum ] = superposition(y, time, dosage_times)
     y_sum = zeros(1, n);
 
     for i = 1:length(dosage_times)
-        nn = nnz((time < dosage_times(i)));
+        nn = nnz(time < dosage_times(i));
         y_sum = y_sum + [zeros(1, nn) y(1:(n-nn))];
     end
 
